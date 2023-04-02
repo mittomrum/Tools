7ecb22c7cdfa062dc581dfb8ff221fec77161482
Fredrik <mittomrum@gmail.com>
2023-04-02 15:49:24 -0700

    gist manifest added for speedy project setup
    https://gist.github.com/mittomrum/54769e89109842169d88029fea3714e1

--- Editor/ToolsMenu.cs
+++ Editor/ToolsMenu.cs
@@ -5,6 +5,8 @@ using static System.IO.Directory;
 using static System.IO.Path;
 using static UnityEngine.Application;
 using static UnityEditor.AssetDatabase;
+using System.Threading.Tasks;
+using System.Net.Http;
 
 namespace tomrum
 {
@@ -16,12 +18,40 @@ namespace tomrum
             CreateDirectories("_Project", "Scripts", "Art", "Scenes");
             Refresh();
         }
-        public static void CreateDirectories(string root, params string[] dir)  {
+        [MenuItem("Tools/Setup/Load Manifest")]
+        static async void LoadNewManifest()
+        {
+            var url = GetGistUrl("54769e89109842169d88029fea3714e1");
+            var contents = await GetContents(url);
+            ReplacePackageFile(contents);
+        }
+
+        //
+
+        public static void CreateDirectories(string root, params string[] dir)
+        {
             var fullPath = Combine(dataPath, root);
             foreach (var newDirectory in dir)
             {
                 CreateDirectory(Combine(fullPath, newDirectory));
             }
+        }
+
+        static string GetGistUrl(string id, string user = "mittomrum") =>
+            $"https://gist.github.com/{user}/{id}/raw";
+
+        static async Task<string> GetContents(string url)
+        {
+            using var client = new HttpClient();
+            var response = await client.GetAsync(url);
+            var content = await response.Content.ReadAsStringAsync();
+            return content;
+        }
+        static void ReplacePackageFile(string contents)
+        {
+            var existing = Path.Combine(Application.dataPath, "../Packages/manifest.json");
+            File.WriteAllText(existing, contents);
+            UnityEditor.PackageManager.Client.Resolve();
         }
     }
 }
